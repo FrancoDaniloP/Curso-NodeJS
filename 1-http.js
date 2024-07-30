cd4af71caaec8ab78c5f146588e8a4645286accd
Danilo Pietropaolo <francodanilo136@gmail.com>
2024-07-30 19:29:03 -0300

    creando rutas

--- 1-http.js
+++ 1-http.js
@@ -3,8 +3,17 @@ const http = require("node:http");
 const desiredPort = process.env.PORT ?? 3000;
 
 const processRequest = (req, res) => {
-  console.log("petición recibida: ", req.url);
-  res.end("Cerrando, recibido.");
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("Biénvenidos");
+  } else if (req.url === "/contacto") {
+    res.statusCode = 200;
+    res.end("Hola soy tal y tal");
+  } else {
+    res.statusCode = 404;
+    res.end("Página no encontrada");
+  }
 };
 
 const server = http.createServer(processRequest);
